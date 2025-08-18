24a21600e352c1889a23251036777c1ae4ae5066
LadyCiaramicoli <l.ciaramicoli@campus.uniurb.it>
2025-08-18 18:26:38 +0200

    Working on json Mission cards

--- src/model/versions/risikockassic/CardManagerRisikoNew.java
+++ src/model/versions/risikockassic/CardManagerRisikoNew.java
@@ -9,6 +9,7 @@ import model.board.IZone;
 import model.card.ICard;
 import model.management.CardManager;
 import model.players.IPlayer;
+import model.versions.risikockassic.card.TerritoryCard;
 
 public class CardManagerRisikoNew extends CardManager {
 	
@@ -22,11 +23,16 @@ public class CardManagerRisikoNew extends CardManager {
 		this.playedCards = new ArrayList<ICard>();
 	}
 	
-	@Override
-	public void initializeGame(List<IPlayer> players) {
-		// TODO Auto-generated method stub
-	}
-	
+//	@Override
+//	public void initializeGame() {
+//		//TODO -- Assegnazione 
+////	    List<IZone> territories = MapManagerRisikoNew.getInstance().getAllTerritories();
+////	    for (IZone territory : territories) {
+////	        territoryCards.add(new TerritoryCard(territory)); // Assicurati che TerritoryCard implementi ICard
+////	    }
+//
+//	}
+
 	@Override
 	public Boolean isReady() {
 		// TODO Auto-generated method stub
@@ -45,4 +51,22 @@ public class CardManagerRisikoNew extends CardManager {
 		
 	}
 	
+	private List<IZone> getTerritoriesList(){
+		return MapManagerRisikoNew.getInstance().getAllTerritories();
+	}
+
+	@Override
+	public void initializeGame(List<IPlayer> players) {
+		// TODO Auto-generated method stub
+		
+	}
+
+//	@Test
+//	public void getTerritoriesList(){
+//		MapManagerRisikoNew.getInstance().initializeGame();
+//		List<IZone> territories = MapManagerRisikoNew.getInstance().getAllTerritories();
+//		System.out.println(territories);
+//
+//	}
+
 }
